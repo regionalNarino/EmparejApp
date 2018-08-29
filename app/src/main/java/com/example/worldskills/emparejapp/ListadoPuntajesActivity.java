8021f602684f13417e4ca43b3e1477487eb16aba
WORLDSKILLS <elhianp28@gmail.com>
2018-08-29 18:26:47 -0500

    nuevos

---
+++ app/src/main/java/com/example/worldskills/emparejapp/ListadoPuntajesActivity.java
@@ -0,0 +1,13 @@
+package com.example.worldskills.emparejapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ListadoPuntajesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_listado_puntajes);
+    }
+}
--- app/src/main/java/com/example/worldskills/emparejapp/PuntajesActivity.java
+++ app/src/main/java/com/example/worldskills/emparejapp/PuntajesActivity.java
@@ -1,7 +1,9 @@
 package com.example.worldskills.emparejapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class PuntajesActivity extends AppCompatActivity {
 
@@ -9,5 +11,16 @@ public class PuntajesActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_puntajes);
+
+
+    }
+
+    public void onClick(View view) {
+        Intent inten=new Intent(PuntajesActivity.this,ListadoPuntajesActivity.class);
+        switch (view.getId()){
+            case R.id.facil:
+
+                break;
+        }
     }
 }
